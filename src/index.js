8d1e47d7753ed018f1bca489eb582a61eca7b855
hao.chen8 <hc222ig@student.lnu.se>
2024-09-03 13:24:23 +0200

    Add functionality to index

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Main entry point of application.
+ */
+
+import { getNameInput } from "./js/components/nameInput";
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Create a name form
+  const form = document.getElementById('name-form')
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault() // Prevent the form from refreshing page
+
+    // Get name input
+    const name = getNameInput()
+
+    // Create an error message
+    const errorMessage = document.getElementById('error-message')
+
+  })
+})
